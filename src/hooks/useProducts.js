7635e47861fbddbbf3fe6e5eeabc3f0789b6f7cf
mdhasan-999 <webdeveloperhasan999@gmail.com>
2022-04-10 22:20:25 +0600

    Problem fixed

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 const useProducts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('Products.json')
+        fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
